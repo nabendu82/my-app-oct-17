0efcf0cb88cddf03f46c82c4901f9ca5c2e774b6
nabendu82 <nabendu.biswas@gmail.com>
2022-10-20 21:24:50 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import ListPost from './components/ListPost';
-// import ParentComponent from './components/ParentComponent';
-// import Counter from './components/Counter';
-// import { Greet, ReactGreet } from './components/Greet';
-// import Greeting from './components/Greeting';
-// import Welcome from './components/Welcome';
+import PostList from './components/PostList';
+// import GrandParent from './components/GrandParent';
+// import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <ReactGreet /> */}
-      {/* <Greeting name="JavaScript" />
-      <Greeting name="Golang">Backend language</Greeting>
-      <Greeting name="Swift" />
-      <hr />
-      <Welcome name="React"/>
-      <Welcome name="Golang">Backend language</Welcome>
-      <Welcome name="React Native" /> */}
-      {/* <Counter /> */}
-      {/* <ParentComponent /> */}
-      <ListPost />
+      {/* <UserProvider value="Nabendu">
+        <GrandParent />
+      </UserProvider> */}
+      <PostList />
     </div>
   );
 }
